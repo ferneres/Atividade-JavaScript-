0065b8ab1b8dbdffafc6ffe70ab830004a747c38
Fernanda Neres <101215657+ferneres@users.noreply.github.com>
2022-12-03 22:37:06 -0300

    Add files via upload

---
+++ Atividade1.js
@@ -0,0 +1,8 @@
+function exibeMsg(){
+    var v1 = document.getElementById("v1").value;
+    var v2 = document.getElementById("v2").value;
+    var v3 = document.getElementById("v3").value;
+
+    var v4 =parseInt(v1) + parseInt(v2)+parseInt(v3) ;
+    alert(v4);
+}
